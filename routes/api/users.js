69d9ec7c4025e0a4a56daef11b54a26805692c4b
chak-eeb <chak_eeb@yahoo.com>
2020-02-05 11:59:32 -0500

    finish route files

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+/* @route    Get api/user
+   @desc     Test route
+   @access   Public  
+*/
+
 router.get('/', (req, res) => {
 	res.send('User route');
 });
+
+module.exports = router;
