41c53c3f291f61bdfadef25df07e2936dec418bc
chak-eeb <chak_eeb@yahoo.com>
2020-02-05 12:45:37 -0500

    finish user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-/* @route    Get api/user
-   @desc     Test route
+/* @route    Post api/user
+   @desc     Register user
    @access   Public  
 */
 
-router.get('/', (req, res) => {
-	res.send('User route');
-});
+router.post(
+	'/',
+	[
+		check('name', 'name is required')
+			.not()
+			.isEmpty(),
+		check('email', 'please enter a valid email').isEmail(),
+		check('password', 'password must be 6 characters or more').isLength({
+			min: 6
+		})
+	],
+	(req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+		res.send('User route');
+	}
+);
 
 module.exports = router;
